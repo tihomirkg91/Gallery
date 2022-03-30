95fa16ba67efc25b2a64481ee513a79b8d2b2e04
tihomirkg91 <tihomirkg91@gmail.com>
2022-03-30 10:44:54 +0200

    version-0.2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 // style
 import index from './index.scss'
@@ -9,11 +9,12 @@ import App from './App'
 
 import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
--- src/local-storage/setDataToLocalStorage.js
+++
@@ -1,9 +0,0 @@
-export const setDataToLocalStorage = (data) => {
-  const arr = []
-  const previousData = JSON.parse(localStorage.getItem('COMMENTED'))
-  arr.unshift(data)
-  if (previousData !== null) {
-    arr.unshift(...previousData)
-  }
-  localStorage.setItem('COMMENTED', JSON.stringify(arr))
-}
